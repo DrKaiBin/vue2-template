1cdc563c9bfe5e9d612ed9c937046d67be3bed9a
DrKaiBin <248997917@qq.com>
2022-03-15 19:17:21 +0800

    添加打包进度条，编译速度时间分析

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,24 @@
  * @Description: vuecli配置
  * @Author: 张楷滨
  * @Date: 2022-03-01 10:51:34
- * @LastEditTime: 2022-03-15 11:06:33
+ * @LastEditTime: 2022-03-15 19:07:25
  * @LastEditors: 张楷滨
  */
 const path = require('path')
+// 打包进度条查看
+const WebpackBar = require('webpackbar')
+
+// 是否开启编译速度时间分析
+const isSpeedMeasure =
+  process.env.NODE_ENV === 'production' &&
+  process.env.VUE_APP_SPEED_MEASURE_WEBPACK === 'true'
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
+const smp = new SpeedMeasurePlugin()
+
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
+
 module.exports = {
   devServer: {
     open: true,
@@ -27,13 +38,23 @@ module.exports = {
   configureWebpack: () => {
     // 插件配置
     const plugins = []
-    return {
+    if (process.env.NODE_ENV === 'production') {
+      plugins.push(new WebpackBar())
+    }
+    // 提炼为变量
+    const _configureWebpack = {
       resolve: {
         alias: {
           '@': resolve('src'),
         },
       },
       plugins,
+    }
+
+    if (isSpeedMeasure) {
+      return smp.wrap(_configureWebpack)
+    } else {
+      return _configureWebpack
     }
   },
   // webpack 链式操作 (高级)
