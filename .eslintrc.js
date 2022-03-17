94c0f0537ac341d7d8169e740ca7c7c8e3f280b4
DrKaiBin <248997917@qq.com>
2022-03-17 19:27:47 +0800

    eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,13 @@
  * @Description: eslint配置
  * @Author: 张楷滨
  * @Date: 2022-03-17 18:38:51
- * @LastEditTime: 2022-03-17 18:39:15
+ * @LastEditTime: 2022-03-17 19:26:52
  * @LastEditors: 张楷滨
  */
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
+    // browser: true,
+    // es2021: true,
   },
   extends: [
     'eslint:recommended',
